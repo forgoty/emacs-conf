e0f6fa00280758efe5a6d450d975bb97ab864a6d
forgoty <forgoty13@gmail.com>
2024-11-05 17:52:19 +0100

    nerd-icons after install guide. Update custom.el

--- modules/emacs-config-packages.el
+++ modules/emacs-config-packages.el
@@ -47,7 +47,7 @@
 (add-to-list 'package-selected-packages 'zig-mode)
 
 ;; UI
-(add-to-list 'package-selected-packages 'nerd-icons)
+(add-to-list 'package-selected-packages 'nerd-icons) ;; after install run M-x nerd-icons-install-fonts
 (add-to-list 'package-selected-packages 'elisp-demos)
 (add-to-list 'package-selected-packages 'helpful)
 (add-to-list 'package-selected-packages 'doom-themes)
